6e517374ad57a7aaccad77f69161a5bda9a200a1
MinhTan <tanbeo31102005@gmail.com>
2026-04-29 08:54:15 +0700

    feat: Add .env configuration for local network testing and setup IP guides

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,12 @@ import 'package:provider/provider.dart';
 import 'package:toeicmobileapp/providers/vocabulary_provider.dart';
 import 'package:toeicmobileapp/ui/screens/home/home_screen.dart';
 
-void main() {
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
   runApp(
     MultiProvider(
       providers: [
